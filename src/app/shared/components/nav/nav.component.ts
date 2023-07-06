25ccc8260d8205e533ca007a31cb3c1a83c831e1
Carina Payleman <rociocarina2@gmail.com>
2023-07-06 20:10:53 -0300

    Crea mobile menú funcional

--- src/app/shared/components/nav/nav.component.ts
+++ src/app/shared/components/nav/nav.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
+  activeMenu = false;
 
+  toggleMenu() {
+    this.activeMenu = !this.activeMenu;
+  }
 }
