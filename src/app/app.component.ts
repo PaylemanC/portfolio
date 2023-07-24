f43929687b01cdabc8862e6249992d0e9edd7051
Carina Payleman <rociocarina2@gmail.com>
2023-07-24 16:03:52 -0300

    Incluye projects data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -80,13 +80,90 @@ export class AppComponent {
 
   projects: Project[] = [
     {
-      id: 0,
-      img: '../../../../assets/imgs/eco-store.png',
+      id: 1,
+      img: '../../../assets/imgs/portfolio.png',
+      title: 'Portfolio Personal',
+      description: '¡Este portafolio! Realicé tanto diseño como programación, aplicando de mis conocimientos en Angular, Figma, Git, usabilidad UX, entre otros, en un reto personal por realizarlo través de todo su proceso desde la planeación hasta su implementación. Está en constante mejora.',
+      skills: ['Angular', 'SCSS', 'TypeScript'],
+      github: 'https://github.com/PaylemanC/myPortfolio',
+      deploy: '', /*********************************************/
+      status: 'IN PROGRESS',
+      category: 'Frontend'
+    },
+    {
+      id: 2,
+      img: '../../../assets/imgs/',
+      title: 'MisRutinas',
+      description: 'Fitness web full stack para creación y seguimiento de rutinas de entrenamiento. Cuenta con sistema de autenticación y CRUD de rutinas y ejercicios. Fue llevada a cabo durante un año junto a un equipo de 7 personas.',
+      skills: ['Angular', 'Django', 'MySQL', 'Bootstrap'],
+      github: 'https://github.com/grupoA7ISPC/ProyectoISPCGrupo55/tree/main',
+      deploy: '',
+      status: 'COMPLETED',
+      category: 'Full Stack'
+    },
+    {
+      id: 3,
+      img: '../../../assets/imgs/', /************************ */
+      title: 'YardSale',
+      description: 'E-commerce de venta de garaje con implementación de routing, carrito y CRUD de productos, conexión con API, etc., iniciado con un maquetado sencillo en HTML&CSS hasta llevarlo al framework de Angular.',
+      skills: ['Angular', 'TypeScript', 'SCSS', 'HTML', 'CSS', 'JavaScript', 'API'],
+      github: 'https://github.com/PaylemanC/YardSale--Platzi',
+      deploy: 'https://yardsale-platzi.netlify.app/',
+      status: 'IN PROGRESS',
+      category: 'Frontend'
+    },
+    {
+      id: 4,
+      img: '../../../assets/imgs/encriptador.png',
+      title: 'Des/Encriptador de Mensajes',
+      description: 'Juego de encriptador y desencriptador de mensajes con un algoritmo de vocales desarrollado en JavaScript para uno de los retos de Oracle Next Education.',
+      skills: ['HTML', 'CSS', 'JavaScript'],
+      github: 'https://github.com/PaylemanC/Encriptador-de-mensajes--ONE',
+      deploy: 'https://encriptador-mensajes.netlify.app/',
+      status: 'COMPLETED',
+      category: 'Frontend'
+    },
+    {
+      id: 5,
+      img: '../../../assets/imgs/pomodoro-playlist.png',
+      title: 'Pomodoro Playlist',
+      description: 'Landing page usando la API de Youtube para traer mi lista de reproducción de vídeos con la técnica de estudio pomodoro que uso para programar y estudiar, con paisajes bonitos y música lofi & piano de fondo.',
+      skills: ['HTML', 'CSS', 'JavaScript', 'API'],
+      github: 'https://github.com/PaylemanC/pomodoro-playlist',
+      deploy: 'https://study-with-me-playlist.netlify.app/',
+      status: 'COMPLETED',
+      category: 'Frontend'
+    },
+    {
+      id: 6,
+      img: '../../../assets/imgs/eco-store.png',
       title: 'Eco-Store',
       description: 'Landing page responsive de un e-commerce con catálogo de productos ecológicos y sustentables provenientes de Colombia.',
       skills: ['HTML', 'CSS', 'SCSS'],
       github: 'https://github.com/PaylemanC/Eco-Store--Platzi',
       deploy: 'https://eco-store-platzi.netlify.app/',
+      status: 'COMPLETED',
+      category: 'Frontend'
+    },
+    {
+      id: 7,
+      img: '../../../assets/imgs/accesible-portfolio.png',
+      title: 'Portfolio Accesible',
+      description: 'Un portfolio sencillo priorizando la accesibilidad web de acuerdo a los lineamientos de la WCAG, incluyendo accesibilidad para usuarios con teclado por navegación por tab y lectores de pantalla.',
+      skills: ['HTML', 'CSS', 'JavaScript'],
+      github: 'https://github.com/PaylemanC/proyEj-portfolio-accesible--Platzi',
+      deploy: 'https://master--portfolio-accesible.netlify.app/index.html',
+      status: 'COMPLETED',
+      category: 'Frontend'
+    },
+    {
+      id: 8,
+      img: '../../../assets/imgs/batatabit.png',
+      title: 'Batatabit',
+      description: 'Landing page responsive de la empresa ficticia de bitcoins Batatabit con enfoque en maquetación mobile first.',
+      skills: ['HTML', 'CSS'],
+      github: 'https://github.com/PaylemanC/Batabit--Platzi',
+      deploy: 'https://batabit-crypto.netlify.app/',
       status: 'COMPLETED',
       category: 'Frontend'
     }
