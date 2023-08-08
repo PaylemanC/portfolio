cf941bda39553130205ea330e0bf8d24098ba55d
Carina Payleman <rociocarina2@gmail.com>
2023-08-08 17:09:09 -0300

    Crea servicio loader con observable inyectado en componente

--- src/app/shared/components/loader/loader.component.ts
+++ src/app/shared/components/loader/loader.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoaderService } from 'src/app/services/loader.service';
 
 @Component({
   selector: 'app-loader',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class LoaderComponent {
 
+  constructor(private loaderService: LoaderService) {}
+  isLoading$ = this.loaderService.isLoading$;
 }
