dd630c729526cc0c9167da4e1f87528fbcdec5a0
Carina Payleman <rociocarina2@gmail.com>
2023-08-16 23:05:15 -0300

    Implementa primer routing a home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { AllProjectsComponent } from './pages/all-projects/all-projects.component';
+import { ProjectDetailComponent } from './pages/project-detail/project-detail.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
