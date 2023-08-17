ee495b7603119a8db542420db1e629d29f8b53ca
Carina Payleman <rociocarina2@gmail.com>
2023-08-16 22:51:11 -0300

    Aplica directamente el service para data

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { PersonalDataService } from '../../../services/personal-data.service';
 
 @Component({
   selector: 'app-footer',
@@ -6,8 +7,19 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
-  @Input() email: string = '';
-  @Input() github = { url: '', user: '' };
-  @Input() linkedin = { url: '', user: '' };
-  @Input() name = '';
+  email: string = '';
+  github = { url: '', user: '' };
+  linkedin = { url: '', user: '' };
+  name = '';
+
+  constructor(private personalDataService: PersonalDataService) { }
+
+  ngOnInit() {
+    this.personalDataService.getAllData().subscribe(data => {
+      this.email = data.social_media.email;
+      this.github = { url: data.social_media.github.url, user: data.social_media.github.user },
+      this.linkedin = { url: data.social_media.linkedin.url, user: data.social_media.linkedin.user };
+      this.name = data.name;
+    });
+  }
 }
