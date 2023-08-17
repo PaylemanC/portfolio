d9fb08dc511d23fae783a1ade98900b1a5fe9395
Carina Payleman <rociocarina2@gmail.com>
2023-08-16 23:57:27 -0300

    Elimina lógica de personalDataService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,66 +14,12 @@ import { Institute } from './models/institute';
 export class AppComponent {
   title = 'myPortfolio';
 
-  personalInfo = { name: '', occupation: '', summary: '', description: '' }
-  social = {
-    github: { url: '', user: '' },
-    linkedin: { url: '', user: '' },
-    email: '',
-  }
-  cv = '';
-  education: Institute[] = []
-  hardSkills: Skill[] = []
-  softSkills: Skill[] = []
-  projects: Project[] = []
-
-  constructor(private personalDataService: PersonalDataService, public loaderService: LoaderService) { }
+  constructor(public loaderService: LoaderService) { }
 
   ngOnInit() {
-    window.onload = () => {
-      this.loaderService.hide();
-    };
-    this.loaderService.show()
-    this.personalDataService.getAllData().subscribe(data => {
-      this.personalInfo = {
-        name: data.name,
-        occupation: data.occupation,
-        summary: data.summary,
-        description: data.description
-      };
-      this.social = {
-        github: { url: data.social_media.github.url, user: data.social_media.github.user },
-        linkedin: { url: data.social_media.linkedin.url, user: data.social_media.linkedin.user },
-        email: data.social_media.email
-      };
-      this.cv = data.cv;
-      this.education = data.education;
-      this.hardSkills = data.skills.hard_skills;
-      this.softSkills = data.skills.soft_skills;
-      this.projects = data.projects;
-      this.loaderService.hide();
-    });
+    // this.loaderService.show()
+    // window.onload = () => {
+    //   this.loaderService.hide();
+    // };
   }
-
-    // {
-    //   "id": 7,
-    //   "img": "./assets/imgs/accesible-portfolio.png",
-    //   "title": "Portfolio Accesible",
-    //   "description": "Un portfolio sencillo priorizando la accesibilidad web de acuerdo a los lineamientos de la WCAG, incluyendo accesibilidad para usuarios con teclado por navegación por tab y lectores de pantalla.",
-    //   "skills": ["HTML", "CSS", "JavaScript"],
-    //   "github": "https://github.com/PaylemanC/proyEj-portfolio-accesible--Platzi",
-    //   "deploy": "https://master--portfolio-accesible.netlify.app/index.html",
-    //   "status": "COMPLETED",
-    //   "category": "Frontend"
-    // },
-    // {
-    //   "id": 8,
-    //   "img": "./assets/imgs/batatabit.png",
-    //   "title": "Batatabit",
-    //   "description": "Landing page responsive de la empresa ficticia de bitcoins Batatabit con enfoque en maquetación mobile first.",
-    //   "skills": ["HTML", "CSS"],
-    //   "github": "https://github.com/PaylemanC/Batabit--Platzi",
-    //   "deploy": "https://batabit-crypto.netlify.app/",
-    //   "status": "COMPLETED",
-    //   "category": "Frontend"
-    // }
 }
