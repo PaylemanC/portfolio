ebb43fa9ecfe37f93d33fd9ddb782d93888bec54
Carina Payleman <rociocarina2@gmail.com>
2024-04-17 12:10:25 -0300

    Elimina módulo proyectos para simplificar abstracción

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProjectsModule } from './projects/projects.module';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { NavComponent } from './shared/components/nav/nav.component';
@@ -10,14 +9,17 @@ import { FooterComponent } from './shared/components/footer/footer.component';
 import { CvDownloadButtonComponent } from './components/cv-download-button/cv-download-button.component';
 import { HardSkillComponent } from './components/hard-skill/hard-skill.component';
 import { SoftSkillComponent } from './components/soft-skill/soft-skill.component';
+import { CardProjectComponent } from './components/card-project/card-project.component';
 import { LoaderComponent } from './shared/components/loader/loader.component';
-import { LoadingInterceptor } from './interceptors/loading.interceptor';
 import { EduItemComponent } from './components/edu-item/edu-item.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ProjectDetailComponent } from './pages/project-detail/project-detail.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { AllProjectsComponent } from './pages/all-projects/all-projects.component';
 import { ScrollTopComponent } from './shared/components/scroll-top/scroll-top.component';
+
+import { LoadingInterceptor } from './interceptors/loading.interceptor';
+import { JoinWithSlashPipe } from './shared/pipes/join-with-slash.pipe';
 
 @NgModule({
   declarations: [
@@ -27,18 +29,19 @@ import { ScrollTopComponent } from './shared/components/scroll-top/scroll-top.co
     CvDownloadButtonComponent,
     HardSkillComponent,
     SoftSkillComponent,
-    LoaderComponent,
+    CardProjectComponent,
     EduItemComponent,
     HomeComponent,
     ProjectDetailComponent,
     NotFoundComponent,
     AllProjectsComponent,
-    ScrollTopComponent
+    ScrollTopComponent,
+    LoaderComponent,
+    JoinWithSlashPipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ProjectsModule,
     HttpClientModule
   ],
   providers: [
--- src/app/components/card-project/card-project.component.ts
+++ src/app/components/card-project/card-project.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { Project } from '../../../models/project';
+import { Project } from 'src/app/models/project';
 
 @Component({
   selector: 'app-card-project',
