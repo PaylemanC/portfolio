7b61c096b104104cc1db578e327ac161e7cf0da5
Carina Payleman <rociocarina2@gmail.com>
2024-06-09 02:02:09 -0300

    Emplea mixin span-icon

--- src/app/components/cv-download-button/cv-download-button.component.ts
+++ src/app/components/cv-download-button/cv-download-button.component.ts
@@ -2,7 +2,10 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-cv-download-button',
-  templateUrl: './cv-download-button.component.html',
+  template: `
+  <a id="cv" [href]="urlCV" download="CV_CarinaPayleman.pdf">
+    <span class="cv-content"><span class="cv-icon"></span>Descargar CV</span>
+  </a>`,
   styleUrls: ['./cv-download-button.component.scss']
 })
 export class CvDownloadButtonComponent {
